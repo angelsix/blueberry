1fa31f977fe2fe7f18ec64d989e58f5aeb37f368
Luke Malpass <contact@angelsix.com>
2019-08-21 10:35:56 +0100

    Add blank iOS and Android apps plus shared UI core

---
+++ Source/Blueberry.Mobile.UI/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Blueberry.Mobile.UI
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

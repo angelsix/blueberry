1a002a679525f66618c12d17a669b88ee9ef6d1e
Luke Malpass <contact@angelsix.com>
2019-08-22 07:58:47 +0100

    Blank WPF Core app ready to go!

---
+++ Source/Blueberry.Desktop.WindowsApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Blueberry.Desktop.WindowsApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Source/Blueberry.Desktop.WindowsApp/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Blueberry.Desktop.WindowsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
